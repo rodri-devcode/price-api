aeaef84e5d7aa711d1ac04423e4050b172024c68
rodri-devcode <manatice@protonmail.com>
2026-06-09 18:42:25 -0300

    Inicio del proyecto:levanta bien

---
+++ src/main/java/ar/com/rodrifernandez/priceapi/PriceApiApplication.java
@@ -0,0 +1,13 @@
+package ar.com.rodrifernandez.priceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/rodrifernandez/priceapi/PriceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.rodrifernandez.priceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
